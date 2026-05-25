ce366c898ec39256611d4fbf48900da1d505b1c8
Archer <3412769+dpull@users.noreply.github.com>
2026-05-25 19:58:24 +0800

    Remove symbolic links in prepare.sh
    Removed symbolic links for various Skynet directories.

--- prepare.sh
+++ prepare.sh
@@ -4,11 +4,4 @@ curl https://www.lua.org/ftp/lua-5.5.0.tar.gz -o lua-5.5.0.tar.gz
 tar -zxvf lua-5.5.0.tar.gz
 rm -rf skynet/3rd/lua
 mv lua-5.5.0/src skynet/3rd/lua
-ln -s -f skynet/3rd/ 3rd
-ln -s skynet/examples/ examples 
-ln -s skynet/lualib/ lualib  
-ln -s skynet/lualib-src/ lualib-src  
-ln -s skynet/service/ service 
-ln -s skynet/service-src/ service-src  
-ln -s skynet/skynet-src/ skynet-src 
-ln -s skynet/test/ test
+
