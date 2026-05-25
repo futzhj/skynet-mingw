6094dc1d9ebcf4b28165efbcf6a4fc5e9ac8f3b0
Archer <3412769+dpull@users.noreply.github.com>
2026-05-25 18:44:51 +0800

    Update Lua path in prepare.sh script

--- prepare.sh
+++ prepare.sh
@@ -3,7 +3,7 @@
 curl https://www.lua.org/ftp/lua-5.5.0.tar.gz -o lua-5.5.0.tar.gz
 tar -zxvf lua-5.5.0.tar.gz
 rm -rf skynet/3rd/lua
-mv lua-5.5.0 skynet/3rd/lua
+mv lua-5.5.0/src skynet/3rd/lua
 ln -s -f skynet/3rd/ 3rd
 ln -s skynet/examples/ examples 
 ln -s skynet/lualib/ lualib  
